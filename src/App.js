e239582fd957659cd968582adc256245f022558b
NongKen <ken@jitta.com>
2022-07-03 19:56:56 +0700

    fix build error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react"
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getDatabase } from "firebase/database";
+// import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBZ5QyZQbF8N2n1vjtqLZW0socKJQAQKiA",
-  authDomain: "it10-three-kingdoms.firebaseapp.com",
-  databaseURL: "https://it10-three-kingdoms-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "it10-three-kingdoms",
-  storageBucket: "it10-three-kingdoms.appspot.com",
-  messagingSenderId: "67551486104",
-  appId: "1:67551486104:web:a18e0cf4b8e1d8061930ed",
-  measurementId: "G-NW98FTTWNE"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBZ5QyZQbF8N2n1vjtqLZW0socKJQAQKiA",
+//   authDomain: "it10-three-kingdoms.firebaseapp.com",
+//   databaseURL: "https://it10-three-kingdoms-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "it10-three-kingdoms",
+//   storageBucket: "it10-three-kingdoms.appspot.com",
+//   messagingSenderId: "67551486104",
+//   appId: "1:67551486104:web:a18e0cf4b8e1d8061930ed",
+//   measurementId: "G-NW98FTTWNE"
+// };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const App = () => {
   const [room, setRoom] = useState(0)
